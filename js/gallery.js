8a96ed858e6bb793c578c84285d7c46ebe9d8df2
Yuri Kovalov <yuri@yurikoval.com>
2015-05-09 20:56:59 +0900

    Add loading indicator

--- js/gallery.js
+++ js/gallery.js
@@ -15,9 +15,10 @@
   };
 
   Gallery = function(options) {
+    this.imageIsLoading = ko.observable(false);
     this.images = ko.observableArray(options.images);
     this.current_image_index = ko.observable(this.images().length - 1);
-    this.current_image = ko.computed((function(_this) {
+    this.current_image = ko.pureComputed((function(_this) {
       return function() {
         return _this.images()[_this.current_image_index()];
       };
@@ -28,12 +29,21 @@
     this.showNext = function() {
       return this.current_image_index(Math.min(this.current_image_index() + 1, this.images().length - 1));
     };
+    this.current_image_index.subscribe((function(_this) {
+      return function() {
+        _this.imageIsLoading(true);
+        return imagesLoaded('#image', function() {
+          return _this.imageIsLoading(false);
+        });
+      };
+    })(this));
     return this;
   };
 
   loadJSON('img/images.json', function(data) {
-    window.images_json = JSON.parse(data);
-    window.gallery = new Gallery(images_json);
+    var gallery, images_json;
+    images_json = JSON.parse(data);
+    gallery = new Gallery(images_json);
     return ko.applyBindings(gallery);
   });
 
--- js/gallery.js.coffee
+++ js/gallery.js.coffee
@@ -12,14 +12,20 @@ loadJSON = (file, callback) ->
   return
 
 Gallery = (options) ->
+  @imageIsLoading = ko.observable(false)
   @images = ko.observableArray(options.images)
   @current_image_index = ko.observable(@images().length-1)
-  @current_image = ko.computed(=> @images()[@current_image_index()])
+  @current_image = ko.pureComputed(=> @images()[@current_image_index()])
   @showPrevious = -> @current_image_index(Math.max(@current_image_index()-1, 0))
   @showNext = -> @current_image_index(Math.min(@current_image_index()+1, @images().length-1))
+
+  @current_image_index.subscribe =>
+    @imageIsLoading(true)
+    imagesLoaded '#image', => @imageIsLoading(false)
+
   return @
 
 loadJSON 'img/images.json', (data) ->
-  window.images_json = JSON.parse data
-  window.gallery = new Gallery(images_json)
+  images_json = JSON.parse data
+  gallery = new Gallery(images_json)
   ko.applyBindings(gallery)
